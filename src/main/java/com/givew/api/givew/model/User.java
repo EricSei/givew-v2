d095dfeb43130fe804297fc6e8ea6c6c892573ca
Ah Hpu Sei <ericsei@Ahs-MacBook-Pro.lan>
2023-07-19 12:47:32 -0400

    project init

---
+++ src/main/java/com/givew/api/givew/model/User.java
@@ -0,0 +1,41 @@
+package com.givew.api.givew.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	private String firstName;
+	private String lastName;
+	
+	public User(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+
+}
